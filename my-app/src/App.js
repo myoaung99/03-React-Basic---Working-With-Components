4d79d2f62f7255681197470b2ed1d7dc1d913751
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-02 09:48:18 +0630

    add dynamic data output

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,10 +24,17 @@ import ExpanseItem from "./components/ExpenseItem";
 // }
 
 function App() {
+  const expanse = [
+    { id: 'e1', title: 'Headphone', amount: '453.12', date: new Date(2022, 2, 2) },
+    { id: 'e2', title: 'Mouse', amount: '200.12', date: new Date(2022, 2, 1) },
+    { id: 'e3', title: 'Keyboard', amount: '100.12', date: new Date(2022, 1, 2) },
+  ];
   return (
     <div>
       <h1>Let's Get Started.</h1>
-      <ExpanseItem />
+      expanse.map(function(e){
+        <ExpanseItem title={e.title} amount={ex.amount} date={e.date.toDateString()} />
+      })
     </div>
   );
 }
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpanseItem.css";
 
-export default function ExpanseItem() {
+export default function ExpanseItem(props) {
+
   return (
     <div className="expanse-item">
-      <div className="expanse-item-date">March 2nd 2022</div>
+      <div className="expanse-item-date">{props.date}</div>
       <div className="expanse-item-des">
-        <h2>Headphone</h2>
-        <div>$645.65</div>
+        <h2>{props.title}</h2>
+        <div>${props.amount}</div>
       </div>
     </div>
   );
