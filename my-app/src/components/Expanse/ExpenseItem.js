0e26a9f05eb754c58a8d9233f9f9c70616cf8872
Myo Myint Aung <myomyintaung064@gmail.com>
2022-03-03 22:46:02 +0630

    add comment on ExchangeItem component

--- my-app/src/components/Expanse/ExpenseItem.js
+++ my-app/src/components/Expanse/ExpenseItem.js
@@ -6,12 +6,16 @@ import "./ExpanseItem.css";
 // props method accept all incoming properties from other Components
 export default function ExpanseItem(props) {
 
+  // useState return the variable and method to change that variable
+  // and useState will re-excute the component where it is used
+  // when the Key variable is changed
   const [title, setTitle] = useState(props.title);
 
+  // Normal EventHandler
   const clickHandler = () => {
+    // method that comes from useState
     setTitle('Updatedg')
   }
-
 
   // HTML liked JSX codes
   return (
